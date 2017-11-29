d24eceb3b53a58b17cf47a5fc4b365a73fc7a2f4
Shyama Menon <shyama12@hotmail.com>
2017-11-29 10:57:45 +0100

    Add tests for GET /todos route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,10 +4,17 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+
+const todos = [{
+	text: "First test todo"
+},{
+	text: "Second test todo"
+}];
+
 beforeEach((done)=>{
 	Todo.remove({}).then(()=>{
-		done();
-	})
+		return Todo.insertMany(todos);
+	}).then(()=>done());
 })
 
 describe('POST/todos',()=>{
@@ -28,7 +35,7 @@ it('should create a new todo',(done)=>{
 			return done(err);
 		}
 
-		Todo.find().then((todos)=>{
+		Todo.find({text}).then((todos)=>{
 			expect(todos.length).toBe(1);
 			expect(todos[0].text).toBe(text);
 			done();
@@ -50,7 +57,7 @@ request(app)
 	}
 
 	Todo.find().then((todos)=>{
-		expect(todos.length).toBe(0);
+		expect(todos.length).toBe(2);
 		done();
 	}).catch((e)=>
 		done(e));
@@ -60,3 +67,15 @@ request(app)
 
 
 });
+
+describe('GET /todos',()=>{
+	it('should get all todos',(done)=>{
+		request(app)
+		.get('/todos')
+		.expect(200)
+		.expect((res)=>{
+			expect(res.body.todos.length).toBe(2);
+		})
+		.end(done);
+	});
+})
