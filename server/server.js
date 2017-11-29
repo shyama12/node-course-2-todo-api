b38247db7eeb28bba06d760db63a3f7fd286ec83
Shyama Menon <shyama12@hotmail.com>
2017-11-29 10:48:52 +0100

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -21,7 +21,20 @@ app.post('/todos', (req,res)=>{
 	}, (e)=>{
 		res.status(400).send(e);
 	});
-})
+});
+
+app.get('/todos',(req,res)=>{
+
+Todo.find().then((todos)=>{
+res.send({
+
+	todos
+});
+},(e)=>{
+  res.status(400).send(e);
+});
+
+});
 
 //GET todos using ids
 
