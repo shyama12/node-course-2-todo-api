717aa794e60ad1196ecf14958fc06c763213e439
Shyama Menon <shyama12@hotmail.com>
2017-11-29 15:26:39 +0100

    Setup for heroku2

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,7 +4,7 @@ mongoose.Promise = global.Promise;
 
 
 //got the mlab url by creating a db in mblab website after creating an account
-mongoose.connect("mongodb://localhost:27017/TodoApp" || "mongodb://shyama12:munnu@ds123926.mlab.com:23926/node-todos-app-db");
+mongoose.connect("mongodb://shyama12:munnu@ds123926.mlab.com:23926/node-todos-app-db" || "mongodb://localhost:27017/TodoApp");
 
 module.exports = {
 	mongoose
